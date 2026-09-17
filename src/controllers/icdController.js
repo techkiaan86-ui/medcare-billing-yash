df18521bfb7b52bf0c9cd538e9a75d04054075d2
Saif-Siddiqui16 <Saif-Siddiqui16@users.noreply.github.com>
2026-09-17 15:26:24 +0530

    done

--- src/controllers/icdController.js
+++ src/controllers/icdController.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../config/db.js';
 
 // Get all ICD codes
 export const getAllICDCodes = async (req, res) => {
