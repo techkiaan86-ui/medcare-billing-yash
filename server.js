bf13831a4b0727168d162a7edc54efe750d37a84
Saif-Siddiqui16 <Saif-Siddiqui16@users.noreply.github.com>
2026-09-17 15:59:42 +0530

    done

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ import attorneyRoutes from './src/routes/attorneyRoutes.js';
 import notificationRoutes from './src/routes/notificationRoutes.js';
 import settingsRoutes from './src/routes/settingsRoutes.js';
 import icdRoutes from './src/routes/icdRoutes.js';
+import modalityRoutes from './src/routes/modalityRoutes.js';
 import { logger } from './src/config/logger.js';
 
 // Load environment variables
@@ -77,6 +78,7 @@ app.use('/v1/staff', staffRoutes);
 app.use('/v1/attorneys', attorneyRoutes);
 app.use('/v1/settings', settingsRoutes);
 app.use('/v1/icd-codes', icdRoutes);
+app.use('/v1/modalities', modalityRoutes);
 
 // Root landing endpoint
 app.get('/', (req, res) => {
